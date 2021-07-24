8305c5fa8ee0daa450c8e3799d7048b817f71b49
mirsamie <m.mirsamie@gmail.com>
2021-07-24 19:09:35 +0430

    log

--- httprouter.go
+++ httprouter.go
@@ -1,6 +1,7 @@
 package httprouter
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -26,14 +27,19 @@ func (receiver Router) DelegatePath(handler http.Handler, path string, method st
 func (receiver Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	method := r.Method
 	path := r.URL.Path
+	fmt.Println("Checking for delegate path : ", method+":"+path)
 	for key, theHandler := range receiver.delegates {
+		fmt.Println("Compare ", key)
 		if strings.HasPrefix(method+":"+path, key) {
+			fmt.Println("OK!")
 			theHandler.ServeHTTP(w, r)
 			return
 		}
 	}
+	fmt.Println("Checking for path : ", method+":"+path)
 	handler, ok := receiver.handlers[method+":"+path]
 	if ok {
+		fmt.Println("OK!")
 		handler.ServeHTTP(w, r)
 	} else {
 		w.WriteHeader(404)
