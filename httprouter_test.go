3c35510a476ea9f6c46b51a6ed8e1a80fa4e7443
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2021-08-05 08:58:34 +0430

    more complicated tests

--- httprouter_test.go
+++ httprouter_test.go
@@ -9,7 +9,7 @@ import (
 func TestHttpRouter_Register(t *testing.T) {
 	var handler http.Handler
 
-	Default.Register(handler, "test", "GET")
+	Default.Register(handler, "/test", "GET")
 
 	tests := []struct {
 		Input struct {
@@ -24,7 +24,7 @@ func TestHttpRouter_Register(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test",
+				Path:   "/test",
 				Method: "GET",
 			},
 			Expected: 0,
@@ -34,7 +34,7 @@ func TestHttpRouter_Register(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test",
+				Path:   "/test",
 				Method: "PUT",
 			},
 			Expected: 405,
@@ -44,7 +44,80 @@ func TestHttpRouter_Register(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test1",
+				Path:   "/test1",
+				Method: "GET",
+			},
+			Expected: 404,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/tes",
+				Method: "GET",
+			},
+			Expected: 404,
+		},
+	}
+
+	for testNumber, test := range tests {
+		_, status := Default.handler(test.Input.Method, test.Input.Path)
+		if status != test.Expected {
+			t.Errorf("Test %d :  %d was expected but got %d", testNumber, test.Expected, status)
+		}
+	}
+}
+
+func TestHttpRouter_RegisterLongPath(t *testing.T) {
+	var handler http.Handler
+
+	Default.Register(handler, "/apple/banana/cherry", "GET")
+
+	tests := []struct {
+		Input struct {
+			Path   string
+			Method string
+		}
+
+		Expected int
+	}{
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/cherry",
+				Method: "GET",
+			},
+			Expected: 0,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/cherry",
+				Method: "PUT",
+			},
+			Expected: 405,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana",
+				Method: "GET",
+			},
+			Expected: 404,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/orange",
 				Method: "GET",
 			},
 			Expected: 404,
@@ -62,7 +135,7 @@ func TestHttpRouter_Register(t *testing.T) {
 func TestHttpRouter_DelegatePath(t *testing.T) {
 	var handler http.Handler
 
-	Default.DelegatePath(handler, "test", "GET")
+	Default.DelegatePath(handler, "/test", "GET")
 
 	tests := []struct {
 		Input struct {
@@ -77,7 +150,7 @@ func TestHttpRouter_DelegatePath(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test",
+				Path:   "/test",
 				Method: "GET",
 			},
 			Expected: 0,
@@ -87,7 +160,7 @@ func TestHttpRouter_DelegatePath(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test",
+				Path:   "/test",
 				Method: "PUT",
 			},
 			Expected: 405,
@@ -97,7 +170,7 @@ func TestHttpRouter_DelegatePath(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test1",
+				Path:   "/tes",
 				Method: "GET",
 			},
 			Expected: 404,
@@ -112,10 +185,218 @@ func TestHttpRouter_DelegatePath(t *testing.T) {
 	}
 }
 
+func TestHttpRouter_DelegateLongPath(t *testing.T) {
+	var handler http.Handler
+
+	Default.DelegatePath(handler, "/apple/banana/cherry", "GET")
+
+	tests := []struct {
+		Input struct {
+			Path   string
+			Method string
+		}
+
+		Expected int
+	}{
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/cherry",
+				Method: "GET",
+			},
+			Expected: 0,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/cherry",
+				Method: "PUT",
+			},
+			Expected: 405,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana",
+				Method: "GET",
+			},
+			Expected: 404,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/orange",
+				Method: "GET",
+			},
+			Expected: 404,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/cherry/dog",
+				Method: "GET",
+			},
+			Expected: 0,
+		},
+	}
+
+	for testNumber, test := range tests {
+		_, status := Default.handler(test.Input.Method, test.Input.Path)
+		if status != test.Expected {
+			t.Errorf("Test %d :  %d was expected but got %d", testNumber, test.Expected, status)
+		}
+	}
+}
+
+func TestHttpRouter_MixedRegisterLongPath(t *testing.T) {
+	var handler http.Handler
+
+	Default.Register(handler, "/apple/banana/cherry", "GET")
+	Default.DelegatePath(handler, "/animals", "GET")
+	Default.DelegatePath(handler, "/apple/basket", "POST")
+
+	tests := []struct {
+		Input struct {
+			Path   string
+			Method string
+		}
+
+		Expected int
+	}{
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/cherry",
+				Method: "GET",
+			},
+			Expected: 0,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/cherry",
+				Method: "PUT",
+			},
+			Expected: 405,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana",
+				Method: "GET",
+			},
+			Expected: 404,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/banana/orange",
+				Method: "GET",
+			},
+			Expected: 404,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/basket",
+				Method: "GET",
+			},
+			Expected: 405,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/basket",
+				Method: "POST",
+			},
+			Expected: 0,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/basket/banana",
+				Method: "POST",
+			},
+			Expected: 0,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/apple/basket/banana/",
+				Method: "POST",
+			},
+			Expected: 0,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/animals",
+				Method: "GET",
+			},
+			Expected: 0,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/animals/dog",
+				Method: "GET",
+			},
+			Expected: 0,
+		},
+		{
+			Input: struct {
+				Path   string
+				Method string
+			}{
+				Path:   "/animals/dog/cat",
+				Method: "GET",
+			},
+			Expected: 0,
+		},
+	}
+
+	for testNumber, test := range tests {
+		_, status := Default.handler(test.Input.Method, test.Input.Path)
+		if status != test.Expected {
+			t.Errorf("Test %d :  %d was expected but got %d", testNumber, test.Expected, status)
+		}
+	}
+}
+
 func TestHttpRouter_Reregister(t *testing.T) {
 	var handler http.Handler
 
-	Default.Register(handler, "test", "GET")
+	Default.Register(handler, "/test", "GET")
 
 	tests := []struct {
 		Input struct {
@@ -130,7 +411,7 @@ func TestHttpRouter_Reregister(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test",
+				Path:   "/test",
 				Method: "POST",
 			},
 			Expected: nil,
@@ -140,7 +421,7 @@ func TestHttpRouter_Reregister(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test1",
+				Path:   "/test1",
 				Method: "GET",
 			},
 			Expected: nil,
@@ -150,7 +431,7 @@ func TestHttpRouter_Reregister(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test",
+				Path:   "/test",
 				Method: "GET",
 			},
 			Expected: errors.New("this path and method are already registered"),
@@ -168,7 +449,7 @@ func TestHttpRouter_Reregister(t *testing.T) {
 func TestHttpRouter_RedelegatePath(t *testing.T) {
 	var handler http.Handler
 
-	Default.DelegatePath(handler, "test", "GET")
+	Default.DelegatePath(handler, "/test", "GET")
 
 	tests := []struct {
 		Input struct {
@@ -183,7 +464,7 @@ func TestHttpRouter_RedelegatePath(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test",
+				Path:   "/test",
 				Method: "POST",
 			},
 			Expected: nil,
@@ -193,7 +474,7 @@ func TestHttpRouter_RedelegatePath(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test1",
+				Path:   "/test1",
 				Method: "GET",
 			},
 			Expected: nil,
@@ -203,7 +484,7 @@ func TestHttpRouter_RedelegatePath(t *testing.T) {
 				Path   string
 				Method string
 			}{
-				Path:   "test",
+				Path:   "/test",
 				Method: "GET",
 			},
 			Expected: errors.New("this path and method are already registered"),
